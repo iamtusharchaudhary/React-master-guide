7f5bada56e83a96504b7d06447d0fe384fb4ed80
Tushar Chaudhary <149557229+iamtusharchaudhary@users.noreply.github.com>
2024-04-22 13:36:19 +0530

    Update example.js

--- React-hooks/UseState-hook/example.js
+++ React-hooks/UseState-hook/example.js
@@ -1,1 +1,23 @@
+import React, { useState } from 'react'
 
+function ExampleOne() {
+    // step 1
+    let data = 0;
+    // setp 2
+    const [count, setCount] = useState(data)
+    let superLike = () => {
+        // setCount(count + 10);
+        for (let i = 1; i <= 50; i++) {
+            setCount(prevCount => prevCount + 1)
+        }
+    }
+    return (
+        <div className='card'>
+            <h1>{count}</h1>
+            <button onClick={() => setCount(count + 1)}>Like</button>
+            <button onClick={() => setCount(0)}>Reset</button>
+            <button onClick={superLike}>super like </button>
+        </div>
+    )
+}
+export default ExampleOne
