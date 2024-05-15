5835b4e1d89bd4cfd46b9be2baa2b759143bb3b5
Tushar Chaudhary <149557229+iamtusharchaudhary@users.noreply.github.com>
2024-05-15 16:20:22 +0530

    Update Demo.js

--- Demo.js
+++ Demo.js
@@ -1,1 +1,29 @@
+// Demo.js main file
 
+import React from 'react'
+import logo from './light-color-smoke-art-2ius9lpteovzjquv.jpg'
+function Demo() {
+  return (
+    <>
+    <div>
+        <h1>Welcome to my first react js code</h1>
+        <img src={logo} alt='banner' width="100%" height="600px"/>
+    </div>
+    </>
+  )
+}
+
+export default Demo
+
+// CSS file
+
+h1{
+  height: 100px;
+  width: 100%;
+  background-color: rgb(194, 141, 73);
+  color: #040404;
+  text-align: center;
+  text-transform: capitalize;
+  align-content: center;
+
+}
