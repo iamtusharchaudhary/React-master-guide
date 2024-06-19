46e3e94e6723298675693f4b771c516708787321
Tushar Chaudhary <149557229+iamtusharchaudhary@users.noreply.github.com>
2024-06-19 16:08:21 +0530

    Add files via upload

---
+++ project/src/netlify-project/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import './Home.css'
+import Home from './Home';
+
+function App() {
+  return (
+    <>
+    <Home/>
+    </>
+  );
+}
+
+export default App;
